5e3be6cb212d7df93c2c333693c85ffbd70707d5
Justin Hoppensteadt <git@justinhoppensteadt.com>
2009-09-30 12:49:17 -0700

    Update from server, add scripts to add ns and update serial

---
+++ addns.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  addns.pl
+#
+#        USAGE:  ./addns.pl  [files]
+#
+#  DESCRIPTION:  will add the nameserver entries to zonefiles and www to root
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Justin Hoppensteadt (JH), Justin.Hoppensteadt@umgtemp.com
+#      COMPANY:  Universal Music Group
+#      VERSION:  1.0
+#      CREATED:  09/30/2009 11:54:34 AM
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+#use re 'debug';
+use File::Slurp;
+
+my $fullrec = <<EONS
+;
+;  Zone NS records
+;
+
+
+@                       NS	ns1.umusic.com.
+ns1.umusic.com.         A	167.167.1.141
+@                       NS	ns2.umusic.com.
+ns2.umusic.com.         A	167.167.9.132
+@                       NS	ns5.umusic.com.
+ns5.umusic.com.         A	167.167.1.132
+
+;
+;  Zone records
+;
+EONS
+  ;
+
+my $partrec = <<EOPNS
+;
+;  Zone NS records
+;
+
+
+@                       NS	ns1.umusic.com.
+EOPNS
+  ;
+
+for my $file (@ARGV) {
+    if ( -e $file ) {
+        my $zone = read_file($file);
+        print $zone;
+        if ( $zone =~ /$fullrec/ ) {
+            next;
+        } else {
+            $zone =~
+              s{(;\s+;\s+Zone NS records\s*;)(.*)(;\s+;\s+Zone records\s*;)}
+            {$fullrec}s;
+        }
+        print $zone;
+    }
+}
+
---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  trytoupdate.sh
+# 
+#         USAGE:  ./trytoupdate.sh 
+# 
+#   DESCRIPTION:  trys zsu if it fails then does zsh -cn
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Justin Hoppensteadt (JH), Justin.Hoppensteadt@umgtemp.com
+#       COMPANY:  Universal Music Group
+#       VERSION:  1.0
+#       CREATED:  09/30/2009 12:43:34 PM PDT
+#      REVISION:  ---
+#===============================================================================
+
+
+
+for FILE in "$@" ; do
+	if [ -e $FILE ]; then
+		if zsu $FILE; then
+			echo "updated $FILE"
+		else
+			if zsu -cn $FILE ; then
+				"updated and new format for $FILE"
+			else
+				echo "COULD NOT FIX $FILE"  
+			fi
+		fi
+	fi
+done
+
